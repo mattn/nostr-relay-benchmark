cead2a340b33249f10aa30fb80a99a48801676fd
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2024-07-23 09:16:24 +0900

    fix script

--- benchmark.sh
+++ benchmark.sh
@@ -7,8 +7,10 @@ RELAY=$1
 function XXX() {
   SEQ=$1
   ID=$(nak event --content "test $SEQ" $RELAY | jq -r .id)
-  nak req -i $ID $RELAY
-  nak event -k 5 -e $ID $RELAY
+  if [ "x$ID" != "x" ]; then
+    nak req -i $ID $RELAY
+    nak event -k 5 -e $ID $RELAY
+  fi
 }
 
 for i in `seq 5`; do XXX $i; done
